8dd5d8cb21c8483c15f4e0f9866e6e8f5b45da10
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-04-16 22:55:07 +0800

    改為read_data_sets

--- simple-RNN.py
+++ simple-RNN.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 # this is data
-mnist = input_data.read_data_set('MNIST_data', one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # hyperparameters
 lr = 0.001
