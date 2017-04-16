08d426cb3fd3befac5e1854d12b6c8c302818913
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-04-17 01:19:18 +0800

    改造為MNIST分類版本

--- LSTM-mnist.py
+++ LSTM-mnist.py
@@ -1,29 +1,33 @@
 import tensorflow as tf
 import numpy as np
 import lib.nn as nn
+from tensorflow.examples.tutorials.mnist import input_data
+
+# this is data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 BATCH_START = 0
-TIME_STEPS = 20
-BATCH_SIZE = 50
-INPUT_SIZE = 1
-OUTPUT_SIZE = 1
+STEPS_SIZE = 28
+BATCH_SIZE = 128
+INPUT_SIZE = 28
+OUTPUT_SIZE = 10
 CELL_SIZE = 10
-LR = 0.006
+LR = 0.001
 
 
 def getBatch():
-    global BATCH_START, TIME_STEPS
-    xs = np.arange(BATCH_START, BATCH_START + TIME_STEPS *
-                   BATCH_SIZE).reshape((BATCH_SIZE, TIME_STEPS)) / (10 * np.pi)
+    global BATCH_START, STEPS_SIZE
+    xs = np.arange(BATCH_START, BATCH_START + STEPS_SIZE *
+                   BATCH_SIZE).reshape((BATCH_SIZE, STEPS_SIZE)) / (10 * np.pi)
 
     seq = np.sin(xs)
     res = np.cos(xs)
-    BATCH_START += TIME_STEPS
+    BATCH_START += STEPS_SIZE
     return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
 
 
 if __name__ == '__main__':
-    model = nn.LSTM(TIME_STEPS, INPUT_SIZE, OUTPUT_SIZE,
+    model = nn.LSTM(STEPS_SIZE, INPUT_SIZE, OUTPUT_SIZE,
                     CELL_SIZE, BATCH_SIZE, LR)
     sess = tf.Session()
     merged = tf.summary.merge_all()
@@ -31,19 +35,36 @@ if __name__ == '__main__':
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    for i in range(200):
-        seq, res, xs = getBatch()
-        if i == 0:
-            feedDict = {model.xs: seq, model.ys: res}
+    step = 0
+    while step * BATCH_SIZE < 100000:
+        batchXs, batchYs = mnsit.train.next_batch(BATCH_SIZE)
+        batchXs = batchXs.reshape([BATCH_SIZE, STEPS_SIZE, INPUT_SIZE])
+        sess.run([model.trainOP], feed_dict={
+            x: batchXs,
+            y: batchYs
+        })
 
-        else:
-            feedDict = {model.xs: seq, model.ys: res,
-                        model.cellInitState: state}
+        if step % 20 == 0:
+            accuracy = tf.reduce_mean(tf.cast(model.prediction, tf.float32))
+            print(sess.run(accuracy, feed_dict={
+                x: batchXs,
+                y: batchYs
+            }))
 
-        _, cost, state, pred = sess.run(
-            [model.trainOP, model.cost, model.cellFinalState, model.prediction], feed_dict=feedDict)
+        step += 1
 
-        if i % 20 == 0:
-            print('cost: ', round(cost, 4))
-            result = sess.run(merged, feedDict)
-            writer.add_summary(result, i)
+    # for i in range(200):
+    #     seq, res, xs = getBatch()
+    #     if i == 0:
+    #         feedDict = {model.xs: seq, model.ys: res}
+    #     else:
+    #         feedDict = {model.xs: seq, model.ys: res,
+    #                     model.cellInitState: state}
+
+    #     _, cost, state, pred = sess.run(
+    #         [model.trainOP, model.cost, model.cellFinalState, model.prediction], feed_dict=feedDict)
+
+    #     if i % 20 == 0:
+    #         print('cost: ', round(cost, 4))
+    #         result = sess.run(merged, feedDict)
+    #         writer.add_summary(result, i)
